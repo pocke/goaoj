ad940cf449e61a8896af87170e839898a7b23cdd
pocke <p.ck.t22@gmail.com>
2014-12-21 19:27:36 +0900

    test

--- problem_list_test.go
+++ problem_list_test.go
@@ -11,4 +11,10 @@ func TestGetProblemList(t *testing.T) {
 	if len(pl) == 0 {
 		t.Error("ProblemList should not be empty. But got empty.")
 	}
+
+	pl, err = api.GetProblemList(83715892)
+	if err == nil {
+		t.Errorf("ProblemList: %+v\n", pl)
+		t.Error("Should return error when does not exists volume. But not return error.")
+	}
 }
